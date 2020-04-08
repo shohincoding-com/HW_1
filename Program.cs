4f0a66224c40d0f02509e5d5eab5fe458735ea87
shohincoding-com <salomov.shokhin@gmail.com>
2020-04-09 01:48:57 +0500

    My first project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HW_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secondSTR = "My first C#_project";
+            Console.WriteLine($"Hello World!\n{secondSTR}");
+        }
+    }
+}
